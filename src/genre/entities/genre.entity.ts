38432c89c9040f26fdfde3bb711f847620378fc1
manjjack <manjjackson@hotmail.com>
2024-04-28 23:46:24 +0100

    genre update

--- src/genre/entities/genre.entity.ts
+++ src/genre/entities/genre.entity.ts
@@ -3,8 +3,7 @@ import { Entity, PrimaryGeneratedColumn, Column, ManyToMany } from 'typeorm';
 
 @Entity()
 export class Genre {
-  //@PrimaryGeneratedColumn()
-  @Column()
+  @PrimaryGeneratedColumn()
   generoId: number;
 
   @Column()
